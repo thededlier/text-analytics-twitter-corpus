3072d3ebd51cb369a2a6b01bacf5cfc615e3edd2
Rohan Anand <thededlier@gmail.com>
2020-03-26 14:05:18 +0000

    output writer

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 import datetime
+import os
 from datetime import timedelta
+from pathlib import Path
+
 from twitterscraper import query_tweets
 
 query_limit = 10
@@ -25,10 +28,21 @@ def generate_query(hashtag=None, date=None, location=None):
     return query
 
 if __name__ == '__main__':
+    hashtag = "#Dublin"
+    location = "Dublin"
+
+    # Make results directory
+    execution_path = Path(__file__).parent.absolute()
+    result_path = execution_path / "results"
+    result_path.mkdir(exist_ok=True)
+    hashtag_result_path = result_path / hashtag.replace("#", '')
+    hashtag_result_path.mkdir(exist_ok=False)
+
+    print("Outputting to " + str(result_path))
 
     date = start_date
     while (date <= end_date):
-        query = generate_query(hashtag="#Dublin", location="Dublin")
+        query = generate_query(hashtag=hashtag, location=location)
 
         list_of_tweets = query_tweets(
             query,
@@ -38,5 +52,19 @@ if __name__ == '__main__':
             lang="en"
         )
 
+        t_iterator = 0
+        date_path = hashtag_result_path / date.strftime("%Y-%m-%d")
+        date_path.mkdir(exist_ok=False)
+
         for tweet in list_of_tweets:
-            print(tweet.text)
+            file_path =  date_path / "{}.txt".format(t_iterator)
+
+            file = open(file_path, "w")
+            file.write(tweet.text)
+            file.close()
+
+            t_iterator += 1
+
+        date = date + timedelta(days=1)
+
+    print("Execution complete!")
